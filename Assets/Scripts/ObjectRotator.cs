17491485cd0f0ab0daaeecd75844ffdd0b12a0fe
jjpaquin <79430973+jjpaquin@users.noreply.github.com>
2022-08-31 17:23:35 -0400

    Completed Level
    Completed the "level" portion of the assignments

--- Assets/Scripts/ObjectRotator.cs
+++ Assets/Scripts/ObjectRotator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ObjectRotator : MonoBehaviour
 {
-    float orbitSpeed = .1f;
+    float orbitSpeed = .5f;
 
     // Start is called before the first frame update
     void Start()
